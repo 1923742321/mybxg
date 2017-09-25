ce3f9c7f17f0c137d0a2ff5c41a3b2181f3c55dd
lisi <1923742321@qq.com>
2017-09-25 11:46:03 +0800

    完成启用注销功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -7,10 +7,36 @@ define(['jquery','template'],function ($,template) {
         url:"/api/teacher",
         dataType:"json",
         success:function (data) {
-        //     console.log(data);
-            var html=template("teacherTpl",{list:data.result})
-            $("#teacherInfo").html(html);
+              // console.log(data);
+             var html=template("teacherTpl",{list:data.result})
+             $("#teacherInfo").html(html);
+             // console.log(2);
+            $(".eod").click(function () {
+                var that=this;
+                var td=$(this).parent();
+                // console.log(td);
+                var tcId=td.attr("data-tcId");
+                var status=td.attr("data-status");
+                // console.log(tcId,status);
+                $.ajax({
+                    type:'post',
+                    url:'/api/teacher/handle',
+                    data:{tc_id:tcId,tc_status:status},
+                    dataType:"JSON",
+                    success:function (data) {
+                        if(data.code==200){
+                            td.attr("data-status",data.result.tc_status);
+                            if(data.result.tc_status==0){
+                                $(that).text("注销");
+                            }else{
+                                $(that).text("启用");
+                            }
+                        }
+                    }
+                })
+            })
         }
-
     })
+    // console.log(1);
+
 })
